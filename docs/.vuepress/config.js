b29d30cf0f2b97b9a5ff735bc5a76bcbd920de76
emmett <xilibi2003@gmail.com>
2019-05-28 14:43:55 +0800

    modify config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  title: "Cosmos SDK Documentation",
-  description: "Documentation for the Cosmos SDK and Gaia.",
+  title: "Cosmos SDK 中文文档|深入浅出区块链",
+  description: "Cosmos SDK 开发手册 & Gaia 中文文档  ",
   ga: "UA-51029217-2",
   dest: "./dist/docs",
-  base: "/docs/",
+  base: "/cosmos/",
   markdown: {
     lineNumbers: true
   },
   themeConfig: {
-    repo: "cosmos/cosmos-sdk",
+    repo: "lbc-team/cosmos-sdk",
     editLinks: true,
     docsDir: "docs",
-    docsBranch: "develop",
-    editLinkText: 'Edit this page on Github',
+    docsBranch: "lbc",
+    editLinkText: '帮助完善文档',
     lastUpdated: true,
     algolia: {
       apiKey: 'a6e2f64347bb826b732e118c1366819a',
@@ -20,7 +20,8 @@ module.exports = {
       debug: false
     },
     nav: [
-      { text: "Back to Cosmos", link: "https://cosmos.network" },
+      { text: "首页", link: "https://learnblockchain.cn" },
+      { text: "En", link: "https://cosmos.network/docs/" },
       { text: "RPC", link: "https://cosmos.network/rpc/" }
     ],
     sidebar: [
@@ -38,21 +39,22 @@ module.exports = {
         collapsable: true,
         children: [
           "/tutorial/",
-          "/tutorial/app-design",
-          "/tutorial/app-init",
-          "/tutorial/keeper",
-          "/tutorial/msgs-handlers",
-          "/tutorial/set-name",
-          "/tutorial/buy-name",
-          "/tutorial/queriers",
-          "/tutorial/codec",
-          "/tutorial/cli",
-          "/tutorial/rest",
-          "/tutorial/app-complete",
-          "/tutorial/entrypoint",
-          "/tutorial/dep",
-          "/tutorial/build-run",
-          "/tutorial/run-rest"
+          "/tutorial/01-app-design",
+          "/tutorial/02-app-init",
+          "/tutorial/03-types",
+          "/tutorial/04-keeper",
+          "/tutorial/05-msgs-handlers",
+          "/tutorial/06-set-name",
+          "/tutorial/07-buy-name",
+          "/tutorial/08-queriers",
+          "/tutorial/09-codec",
+          "/tutorial/10-cli",
+          "/tutorial/11-rest",
+          "/tutorial/12-app-complete",
+          "/tutorial/13-entrypoint",
+          "/tutorial/14-dep",
+          "/tutorial/15-build-run",
+          "/tutorial/16-run-rest"
         ]
       },
       {
